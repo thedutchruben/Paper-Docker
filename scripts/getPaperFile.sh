510b096df16c3b92e961aba776144cb864eecbff
Ruben de Roos <deroosruben@gmail.com>
2022-08-18 15:36:54 +0200

    Update getPaperFile.sh

--- scripts/getPaperFile.sh
+++ scripts/getPaperFile.sh
@@ -3,7 +3,6 @@ result=$(curl --header "Accept: */*" "https://api.papermc.io/v2/projects/paper/v
 echo $result > /versions.txt
 build=$(sed '1s/array =//' /versions.txt | jq '.builds | last')
 echo Found build : $build
-echo "https://api.papermc.io/v2/projects/paper/versions/$version/builds/$build/downloads/paper-$version-$build.jar"
 curl --header "Accept: */*" "https://api.papermc.io/v2/projects/paper/versions/$version/builds/$build/downloads/paper-$version-$build.jar" --output "/minecraft/paper.jar"
 sh /scripts/getViaversion.sh
 sh /scripts/startPaperServer.sh
